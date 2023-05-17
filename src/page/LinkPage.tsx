bc5cb24c78ccbf12aaca2e064fe659bfe438d5d3
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-05-17 11:51:33 +0800

    feat: send email in linkPage

--- src/page/LinkPage.tsx
+++ src/page/LinkPage.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { sendMail } from '../server/aixos'
 
 function LinkPage (): JSX.Element {
+  const [value, setValue] = useState<string>('')
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(e.target.value)
+  }
+  const testMail = (): void => {
+    sendMail(value).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
   return (
+
     <div>
       <p className="text-5xl">hello world</p>
-      <input></input>
+      <input value={value} onInput={handleInput}/>
+      <button onClick={testMail}>send</button>
+
       <Link to="/">back</Link>
     </div>
   )
--- src/server/aixos.ts
+++ src/server/aixos.ts
@@ -4,4 +4,11 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
-export const example = async (): Promise<AxiosResponse<any, any>> => { return await instance.post('/path', 'formdata') }
+export const sendMail = async (mail: string): Promise<AxiosResponse<any, any>> => {
+  return await instance.get('/mail/invite', {
+    params: {
+      token: process.env.REACT_APP_TOKEN,
+      email: mail
+    }
+  })
+}
