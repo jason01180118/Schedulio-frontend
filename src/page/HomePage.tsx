0cde70aea4d0c7480ffc24443257c990f6d3fe2e
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-05-17 10:48:35 +0800

    feat: change button to link

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import background from '../assets/background.jpg'
+import { Link } from 'react-router-dom'
 
 function HomePage (): JSX.Element {
   return (
     <div className='absolute w-full h-full'>
       <img className='absolute w-full h-full z-[-1]' src={background}/>
       <div className='absolute w-full h-full flex justify-center items-center'>
-        <button>123</button>
-        <button>456</button>
+        <Link to='/calendar' className='w-[20%] h-[10%] mx-12 rounded-2xl border-blue-500 border-sm'>calendar</Link>
+        <Link to='/link' className='w-[20%] h-[10%] mx-12 rounded-2xl border-blue-500 border-sm'>link</Link>
       </div>
     </div>
   )
