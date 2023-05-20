4eaef1f799c2e0f6fa80d45f611e8359d9c59f46
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-05-21 00:11:50 +0800

    style: change margin and input value

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -12,21 +12,23 @@ function SignPage (): JSX.Element {
     }
     const account = target.account.value
     const password = target.password.value
-    logIn(account, password).catch((err) => {
+    logIn(account, password).then(() => {
+      setErrMsg('success')
+    }).catch((err) => {
       setErrMsg(err.message)
     })
   }
   return (
     <div className='absolute w-full h-[92%] flex flex-col justify-center items-center'>
       <p className='fontsize-bigtitle font-Alata mb-4'>sign in</p>
-      <form className='w-[40%] h-[60%] bg-white rounded-xl flex flex-col items-center justify-center' onSubmit={handleSubmit}>
-        <label className='fontsize-title font-Alata mb-8' htmlFor='account'>account</label>
-        <input className='fontsize-title font-Alata mb-8 border' name='account' type='text'></input>
-        <label className='fontsize-title font-Alata mb-8' htmlFor='password'>password</label>
-        <input className='fontsize-title font-Alata mb-8 border' name='password' type='password'></input>
-        <input className='fontsize-title font-Alata mb-8' type='submit'></input>
-        <Link to="/register" className='fontsize-title font-Alata mb-8'>register</Link>
-        <p className='fontsize-title font-Alata mb-8'>{errMsg}</p>
+      <form className='w-[40%] h-[50%] bg-white rounded-xl flex flex-col items-center justify-center' onSubmit={handleSubmit}>
+        <label className='fontsize-title font-Alata mb-4' htmlFor='account'>account</label>
+        <input className='fontsize-title font-Alata mb-4 border' name='account' type='text'></input>
+        <label className='fontsize-title font-Alata mb-4' htmlFor='password'>password</label>
+        <input className='fontsize-title font-Alata mb-4 border' name='password' type='password'></input>
+        <input className='fontsize-title font-Alata mb-4 cursor-pointer' type='submit' value="login"></input>
+        <Link to="/register" className='fontsize-title font-Alata mb-4'>register</Link>
+        <p className='fontsize-title text-red-600 font-Alata mb-4'>{errMsg}</p>
       </form>
 
     </div>
--- src/page/RegisterPage.tsx
+++ src/page/RegisterPage.tsx
@@ -9,24 +9,34 @@ function RegisterPage (): JSX.Element {
     const target = e.target as typeof e.target & {
       account: { value: string }
       password: { value: string }
+      passwordAgain: { value: string }
     }
     const account = target.account.value
     const password = target.password.value
-    logIn(account, password).catch((err) => {
-      setErrMsg(err.message)
-    })
+    const passwordAgain = target.passwordAgain.value
+    if (password === passwordAgain) {
+      logIn(account, password).then(() => {
+        setErrMsg('success')
+      }).catch((err) => {
+        setErrMsg(err.message)
+      })
+    } else {
+      setErrMsg('password err')
+    }
   }
   return (
     <div className='absolute w-full h-[92%] flex flex-col justify-center items-center'>
-      <p className='fontsize-bigtitle font-Alata mb-4'>sign in</p>
-      <form className='w-[40%] h-[60%] bg-white rounded-xl flex flex-col items-center justify-center' onSubmit={handleSubmit}>
-        <label className='fontsize-title font-Alata mb-8' htmlFor='account'>account</label>
-        <input className='fontsize-title font-Alata mb-8 border' name='account' type='text'></input>
-        <label className='fontsize-title font-Alata mb-8' htmlFor='password'>password</label>
-        <input className='fontsize-title font-Alata mb-8 border' name='password' type='password'></input>
-        <input className='fontsize-title font-Alata mb-8' type='submit'></input>
-        <Link to="/login" className='fontsize-title font-Alata mb-8'>login</Link>
-        <p className='fontsize-title font-Alata mb-8'>{errMsg}</p>
+      <p className='fontsize-bigtitle font-Alata mb-4'>register</p>
+      <form className='w-[40%] h-[70%] bg-white rounded-xl flex flex-col items-center justify-center' onSubmit={handleSubmit}>
+        <label className='fontsize-title font-Alata mb-4' htmlFor='account'>account</label>
+        <input className='fontsize-title font-Alata mb-4 border' name='account' type='text'></input>
+        <label className='fontsize-title font-Alata mb-4' htmlFor='password'>password</label>
+        <input className='fontsize-title font-Alata mb-4 border' name='password' type='password'></input>
+        <label className='fontsize-title font-Alata mb-4' htmlFor='passwordAgain'>passwordAgain</label>
+        <input className='fontsize-title font-Alata mb-4 border' name='passwordAgain' type='password'></input>
+        <input className='fontsize-title font-Alata mb-4 cursor-pointer' type='submit' value="create"></input>
+        <Link to="/login" className='fontsize-title font-Alata mb-4'>login</Link>
+        <p className='fontsize-title text-red-600 font-Alata mb-4'>{errMsg}</p>
       </form>
 
     </div>
