8e2f6cd601495e880147bbc362e3a3f0ec593db0
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-05-23 06:07:25 +0800

    feat: add toolbar in calendar

--- src/page/CalendarPage.tsx
+++ src/page/CalendarPage.tsx
@@ -4,25 +4,32 @@ import { ViewState } from '@devexpress/dx-react-scheduler'
 import {
   Scheduler,
   WeekView,
-  Appointments
+  Appointments,
+  Toolbar,
+  DateNavigator,
+  TodayButton
 } from '@devexpress/dx-react-scheduler-material-ui'
 
 function CalendarPage (): JSX.Element {
-  const currentDate = '2018-11-01'
   const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-    { startDate: '2018-11-02T12:00', endDate: '2018-11-02T13:30', title: 'Go to a gym' }
+    { startDate: '2023-05-23T09:45', endDate: '2023-05-23T11:00', title: 'Meeting' },
+    { startDate: '2023-05-23T12:00', endDate: '2023-05-23T13:30', title: 'Go to a gym' },
+    { startDate: '2023-05-24T12:00', endDate: '2023-05-24T13:30', title: 'Go to a gym' }
   ]
+
   return (
     <div className='absolute w-full h-[92%] flex justify-center items-center'>
       <Paper className='w-[80%] h-[70%]'>
         <Scheduler data={schedulerData}>
-      <ViewState currentDate={currentDate}/>
-      <WeekView startDayHour={9} endDayHour={14}/>
-      <Appointments />
-    </Scheduler>
-  </Paper>
+          <ViewState defaultCurrentDate={new Date()}/>
+          <WeekView startDayHour={9} endDayHour={24}/>
+          <Toolbar />
+          <DateNavigator />
+          <TodayButton />
+          <Appointments />
+
+        </Scheduler>
+      </Paper>
     </div>
   )
 }
