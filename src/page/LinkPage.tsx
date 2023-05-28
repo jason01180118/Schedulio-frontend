00746a13a84793cc6807d06a5d5e6dbaf9307001
alicechen0312 <chenshin910312@gmail.com>
2023-05-28 13:18:29 +0800

    link page css

--- src/page/LinkPage.tsx
+++ src/page/LinkPage.tsx
@@ -18,7 +18,7 @@ function LinkPage (): JSX.Element {
 
     <form className='absolute w-full h-[92%] flex flex-col justify-center items-center' onSubmit={testMail}>
       <p className='my-2 text-3xl font-Alata'>Input others Gmail to get their calendar</p>
-      <input className='font-Alata my-2 w-[80%] h-[6%] rounded-1xl bg-white bg-opacity-50' value={value} onInput={handleInput}/>
+      <input className='font-Alata my-2 w-[50%] h-[6%] rounded-1xl bg-white bg-opacity-50' value={value} onInput={handleInput}/>
       {/* <span className="w-[70%] h-1 bg-gray-400"></span> */}
       <input type='submit' className='w-24 my-2 text-3xl font-Alata cursor-pointer bg-green-200 rounded-3xl shadow-lg' value="Send"></input>
     </form>
