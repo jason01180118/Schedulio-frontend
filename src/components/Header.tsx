faeb614024352ef0cfe00b050db301bdf7480c79
pudding0803 <nakohui160@gmail.com>
2023-05-28 13:21:06 +0800

    rename token to session

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
 
 function Header (): JSX.Element {
-  const [cookies,, removeCookie] = useCookies(['token'])
+  const [cookies,, removeCookie] = useCookies(['session'])
   const [login, setLogin] = useState(false)
   const links = [
     { title: 'My Calendar', to: '/calendar' },
@@ -11,12 +11,12 @@ function Header (): JSX.Element {
 
   ]
   useEffect(() => {
-    if (cookies.token !== undefined) {
+    if (cookies.session !== undefined) {
       setLogin(true)
     } else {
       setLogin(false)
     }
-  }, [cookies.token])
+  }, [cookies.session])
 
   return (
     <ul className="h-[8%] w-full border-b shadow-sm flex items-center justify-between">
@@ -26,7 +26,7 @@ function Header (): JSX.Element {
       </ul>
     {
       login
-        ? <button className='w-[7%] h-[50%] fontsize-content font-Alata mx-12 flex justify-center items-center bg-green-200 rounded-3xl' onClick={() => { removeCookie('token'); console.log(cookies.token) }}>Log Out</button>
+        ? <button className='w-[7%] h-[50%] fontsize-content font-Alata mx-12 flex justify-center items-center bg-green-200 rounded-3xl' onClick={() => { removeCookie('session'); console.log(cookies.session) }}>Log Out</button>
         : <Link className='w-[6%] h-[50%] fontsize-content font-Alata mx-12 flex justify-center items-center bg-green-200 rounded-3xl' to="/login">Sign In</Link>
 
     }
